a7e103cc31a97b9739fc9c8af8d1652a90d94b61
zxwanderer <wanderer@amberskynet.org>
2020-12-22 21:48:29 +0300

    some updates

--- dynamic.asm
+++ dynamic.asm
@@ -7,8 +7,9 @@ MAP_SET:
 	; include "data/maps/laboratory.asm"
   ; include "data/maps/laboratory_old.asm"
   ; include "data/maps/laboratory_demo.asm"
-  include "data/maps/laboratory3.asm"
+  ; include "data/maps/laboratory3.asm"
   ; include "data/maps/test_lab.asm"
+  include "data/map/cells.asm"
 
 MAP_SET_END
   include "data/heroes.asm"
