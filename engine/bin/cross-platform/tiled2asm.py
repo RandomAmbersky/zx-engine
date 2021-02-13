3ec7b74af1173e0d975d6f52395c0746aaee5014
zxwanderer <no@mail.me>
2021-02-13 18:49:23 +0300

    try add some tiled2asm.py

---
+++ engine/bin/cross-platform/tiled2asm.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+
+
+
+
+def createparser():
+    myparser = argparse.ArgumentParser()
+    myparser.add_argument('-i', '--input', type=str)
+    myparser.add_argument('-o', '--output', default='cells.asm', type=str)
+    return myparser
+
+
+def error(message):
+    print(message, file=sys.stderr)
+    exit(1)
+
+
+def main():
+    parser = createparser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
